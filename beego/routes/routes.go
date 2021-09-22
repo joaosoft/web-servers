3254956b115d1fdf97facb4a7dc3dbc5b71ca9f8
João Ribeiro <joao.ribeiro@foursource.com>
2021-09-22 09:07:50 +0100

    errors

--- beego/routes/routes.go
+++ beego/routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"encoding/json"
+	"net/http"
 	"web-servers/beego/controllers"
 	"web-servers/beego/middlewares"
 
@@ -12,4 +14,17 @@ func Init(router *beego.App) {
 	router.Handlers.Add("/v1/persons/:id_person", &controllers.PersonController{}, "get:GetPersonByID")
 	router.Handlers.Add("/v1/persons/:id_person/addresses/:id_address", &controllers.AddressController{}, "get:GetPersonAddressByID")
 	router.Handlers.Add("/v1/errors", &controllers.ErrorController{}, "get:GetErrorByID")
+
+	beego.ErrorHandler("404",
+		func(w http.ResponseWriter, r *http.Request) {
+			bytes, _ := json.Marshal(struct {
+				Code  int    `json:"code"`
+				Error string `json:"error"`
+			}{
+				Code:  http.StatusNotFound,
+				Error: http.StatusText(http.StatusNotFound),
+			})
+			_, _ = w.Write(bytes)
+		},
+	)
 }
--- gin/routes/routes.go
+++ gin/routes/routes.go
@@ -15,4 +15,14 @@ func Init(router *gin.Engine) {
 	v1.Handle(http.MethodGet, "/persons/:id_person", controllers.GetPersonByID)
 	v1.Handle(http.MethodGet, "/persons/:id_person/addresses/:id_address", controllers.GetPersonAddressByID)
 	v1.Handle(http.MethodGet, "/errors", controllers.GetErrorByID)
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, struct {
+			Code  int    `json:"code"`
+			Error string `json:"error"`
+		}{
+			Code:  http.StatusNotFound,
+			Error: http.StatusText(http.StatusNotFound),
+		})
+	})
 }
